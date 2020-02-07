df9106411a8778ad9b1ab261df44d35339c390f2
Mitchell Fenner <mitchfen@protonmail.com>
2020-02-06 19:18:42 -0500

    Broke into functions, now can sanitize song names

--- renameMyMusic.py
+++ renameMyMusic.py
@@ -16,27 +16,49 @@ import subprocess
 from pathlib import Path
 from colorama import Fore, Style
 
-# Ask user where the music is stored
-print (Fore.GREEN + "\nHello, I will help you rename your music.")
-print ("Please enter the directory your music is in.")
-print ("Enter it like: /home/username/music\n")
-print(Style.RESET_ALL)
-print (Fore.BLUE + "Directory: ", end = "")   # want input on same line, different color
-print(Style.RESET_ALL, end = "")
-musicDir = input()
+def rename(pathToSong, newName):            
+    print(Fore.RED + "Renaming to " + newName)    # want to confirm rename in red
+    print(Style.RESET_ALL)
+    cmd = ("mv " + pathToSong + " " + newName)
+    os.system(cmd)
 
-#Ensure directory is valid and ends in a backslash
-if musicDir[len(musicDir)-1] != "/":
-    musicDir += "/"
-assert os.path.exists(musicDir), "ERROR "+str(musicDir) + " is an invalid directory"
+def getMusicDirectory():
+    # Ask user where the music is stored
+    print (Fore.GREEN + "\nHello, I will help you rename your music.")
+    print ("Please enter the directory your music is in.")
+    print ("Enter it like: /home/username/music\n")
+    print(Style.RESET_ALL)
+    print (Fore.BLUE + "Directory: ", end = "")   # want input on same line, different color
+    print(Style.RESET_ALL, end = "")
+    musicDir = input()
+     #Ensure directory is valid and ends in a backslash
+    if musicDir[len(musicDir)-1] != "/":
+        musicDir += "/"
+    assert os.path.exists(musicDir), "ERROR "+str(musicDir) + " is an invalid directory"
+    return musicDir
+
+def getAndCheckSongName(nameToCheck):
+    # ensure that name the user wants does not contain weird characters
+    # only spaces and alphanumerics are allowed
+    # multiple spaces are allowed but seriously.. don't do that
+    keepGoing = True
+    while(keepGoing):
+        y = re.search("^[a-zA-Z0-9\s]+$", nameToCheck)
+        if(y):
+            keepGoing = False
+        else:
+            print("Error this is not a valid name")
+            nameToCheck = input("Please try again: ")
+    return nameToCheck
+            
+musicDir = getMusicDirectory()
+files = []
 
 # Walk that directory and build array of files
-files = []
+# Then iterate through files array and use regex to replace " " with "\ " so we can navigate directories
+# Example: /home/mitch/music/song\ with\ spaces.mp3
 for (path, dirnames, filenames) in os.walk(musicDir):
     files.extend(os.path.join(path, name) for name in sorted(filenames))
-
-# Iterate through files array and use regex to replace " " with "\ " so we can navigate directories
-# Example: /home/mitch/music/song\ with\ spaces.mp3
 for i in range (len(files)):
     tempString = str(files[i])
     files[i] = re.sub("\s", "\ ", tempString)
@@ -47,22 +69,22 @@ for i in range(len(files)):
     os.system("play " + files[i])
     print(Style.RESET_ALL)
     print("Name this file(x to delete, k to keep) do not include extension.")
-    inputName = input("Name: ")
-    if inputName == "x":
-            #os.remove(files[i])    This fails when file name has spaces, unsure why
+    uncheckedName = input("Name: ")
+    songName = getAndCheckSongName(uncheckedName)
+    if songName== "x":
             cmd = ("rm " + files[i])
             os.system(cmd)      # this is always successful, albiet with risky rm command
             print(Fore.RED + "Deleted.")
             print(Style.RESET_ALL)
-    elif inputName == "k":
+    elif songName == "k":
             print(Fore.RED + "Keeping file as " + files[i])
             print(Style.RESET_ALL)
     else:
         if files[i][len(files[i])-1] == "3": # mp3 file
-            inputName = musicDir + inputName + ".mp3"
+            songName = musicDir + songName + ".mp3"
+            rename(files[i], songName)
         elif files[i][len(files[i])-1] == "v": # wav file
-            inputName = musicDir + inputName + ".wav"
-        print(Fore.RED + "Renaming to " + inputName)    # want to confirm rename in red
-        print(Style.RESET_ALL)
-        cmd = ("mv " + files[i] + " " + inputName)
-        os.system(cmd)
+            songName = musicDir + songName + ".wav"
+            rename(files[i], songName)
+        else:
+           print(Fore.RED + "Renaming to") 
