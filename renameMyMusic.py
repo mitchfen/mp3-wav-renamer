0aaf4121c1a882856b6b0b3a22f81e83332f7325
Mitchell Fenner <mitchfen@protonmail.com>
2020-02-06 19:47:39 -0500

    Can now handle special characters in song names

--- renameMyMusic.py
+++ renameMyMusic.py
@@ -8,7 +8,6 @@ add mechanism to detect if sox is installed on Manjaro
 -Add convenient way to count songs in directory and see progress working through them
 -Right now it assumes all files are mp3, need more code to deal with wav files
 '''
-
 import re
 import os
 import sys
@@ -19,7 +18,8 @@ from colorama import Fore, Style
 def rename(pathToSong, newName):            
     print(Fore.RED + "Renaming to " + newName)    # want to confirm rename in red
     print(Style.RESET_ALL)
-    cmd = ("mv " + pathToSong + " " + newName)
+    # mv bash command must recieve two arguments in quotes
+    cmd = ("mv " + "\"" + pathToSong + "\"" + " " + "\"" + newName + "\"")
     os.system(cmd)
 
 def getMusicDirectory():
@@ -50,41 +50,45 @@ def getAndCheckSongName(nameToCheck):
             print("Error this is not a valid name")
             nameToCheck = input("Please try again: ")
     return nameToCheck
+
+def playAndTakeInput(files = []):
+    for i in range(len(files)):
+        print (Fore.YELLOW) # I want the sox output to be yellow so I can distinguish from my python
+        os.system("play " + "\"" + files[i] + "\"")
+        print(Style.RESET_ALL)
+        print("Name this file(x to delete, k to keep) do not include extension.")
+        uncheckedName = input("Name: ")
+        songName = getAndCheckSongName(uncheckedName)
+        if songName== "x":
+                cmd = ("rm " + files[i])
+                os.system(cmd)      # this is always successful, albiet with risky rm command
+                print(Fore.RED + "Deleted.")
+                print(Style.RESET_ALL)
+        elif songName == "k":
+                print(Fore.RED + "Keeping file as " + files[i])
+                print(Style.RESET_ALL)
+        else:
+            if files[i][len(files[i])-1] == "3": # mp3 file
+                songName = musicDir + songName + ".mp3"
+                rename(files[i], songName)
+            elif files[i][len(files[i])-1] == "v": # wav file
+                songName = musicDir + songName + ".wav"
+                rename(files[i], songName)
+            else:
+                print(Fore.RED + "Renaming to") 
             
+def collectSongs(musicDir, files = []):
+    # Walk the directory and build array of files
+    # Then iterate through files array and use regex to replace " " with "\ " so we can navigate directories
+    # Example: /home/mitch/music/song\ with\ spaces.mp3
+    for (path, dirnames, filenames) in os.walk(musicDir):
+        files.extend(os.path.join(path, name) for name in sorted(filenames))
+    for i in range (len(files)):
+        tempString = str(files[i])
+        files[i] = re.sub("\s", "\ ", tempString)
+    return files
+
 musicDir = getMusicDirectory()
 files = []
-
-# Walk that directory and build array of files
-# Then iterate through files array and use regex to replace " " with "\ " so we can navigate directories
-# Example: /home/mitch/music/song\ with\ spaces.mp3
-for (path, dirnames, filenames) in os.walk(musicDir):
-    files.extend(os.path.join(path, name) for name in sorted(filenames))
-for i in range (len(files)):
-    tempString = str(files[i])
-    files[i] = re.sub("\s", "\ ", tempString)
-
-# Play song until keyboard interupt
-for i in range(len(files)):
-    print (Fore.YELLOW) # I want the sox output to be yellow so I can distinguish from my python
-    os.system("play " + files[i])
-    print(Style.RESET_ALL)
-    print("Name this file(x to delete, k to keep) do not include extension.")
-    uncheckedName = input("Name: ")
-    songName = getAndCheckSongName(uncheckedName)
-    if songName== "x":
-            cmd = ("rm " + files[i])
-            os.system(cmd)      # this is always successful, albiet with risky rm command
-            print(Fore.RED + "Deleted.")
-            print(Style.RESET_ALL)
-    elif songName == "k":
-            print(Fore.RED + "Keeping file as " + files[i])
-            print(Style.RESET_ALL)
-    else:
-        if files[i][len(files[i])-1] == "3": # mp3 file
-            songName = musicDir + songName + ".mp3"
-            rename(files[i], songName)
-        elif files[i][len(files[i])-1] == "v": # wav file
-            songName = musicDir + songName + ".wav"
-            rename(files[i], songName)
-        else:
-           print(Fore.RED + "Renaming to") 
+collectSongs(musicDir, files)
+playAndTakeInput(files)
