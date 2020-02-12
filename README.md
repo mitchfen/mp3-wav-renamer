9ec712b8843b9402c9d069802974e417daa8dbba
Mitchell Fenner <mitchfen@protonmail.com>
2020-02-11 19:25:38 -0500

    Refactoring, updated readme, simplified imports

--- README.md
+++ README.md
@@ -2,17 +2,24 @@
 
 I am writing this python script to help me organize my mess of a music library.  
 
-This program reads each mp3 or wav into a list, then iterates through them and plays each one until a keyboard interupt.
+This program reads each mp3 or wav into a list, then iterates through them and plays each one until a keyboard interupt (CTRL + C).
 
-Then the user is asked to rename the song, delete it, or keep it.
+The user is then asked to rename the song, delete it, or keep it.
+
+## Dependencies
+
+Colorama:  `pip install colorama`  
+SoX: Debian and derivatives: `sudo apt-get install sox` or `sudo pacman -S sox` on Manjaro  
+
+## TODO
+
+* Windows support: SoX not workable on windows.
+    * Have tried VLC without GUI but need way to cancel playback
+* Need to be able to detect redundant names to avoid overwrite
+* Add functionality to move to sub directories
+* Add output to see progress
 
 ## Screenshot
 
 ![broken-link](https://github.com/mitchfen/renameMyMusic/blob/master/screenshots/screen1.png)
 
-### Dependencies
-
-Colorama:  `pip install colorama`  
-SoX: Ubuntu and derivatives use `sudo apt-get install sox` or `sudo pacman -S sox` on Manjaro  
-
-VLC if you are running the script on Windows  [VLC](https://www.videolan.org/vlc/)
--- renameMyMusic.py
+++ renameMyMusic.py
@@ -1,25 +1,11 @@
-''' TODO
-- add windows support - sox setup not simple on windows, need alternative
-- need to be able to detect redundant names to avoid overwrite
-- add mechanism to detect if sox is installed before playing music
-- add functionality to move to sub directories within Music folder
-- add output to see how many songs exist and how many have been renamed
-'''
-import platform
-import re
 import os
 import sys
-import subprocess
 from pathlib import Path
+from platform import system
+from re import search
 from colorama import Fore, Style, deinit, init
 
 init() # initialize colorama
-def renameSong(pathToSong, newName):            
-    print(Fore.RED + "Renaming to " + newName)    # want to confirm rename in red
-    print(Style.RESET_ALL)
-    # mv bash command must recieve two arguments in quotes
-    cmd = ("mv " + "\"" + pathToSong + "\"" + " " + "\"" + newName + "\"")
-    os.system(cmd)
 
 def getMusicDirectory():
     # Ask user where the music is stored
@@ -32,15 +18,74 @@ def getMusicDirectory():
     musicDir = input()
     # Ensure directory terminates in correct symbol.
     # Need a / on Unix, \ for Windows
-    if platform.system() == 'Windows':
+    if system() == 'Windows':
         if musicDir[len(musicDir)-1] != "\"":
             musicDir += "/"
-    if platform.system() == 'Linux':
+    if system() == 'Linux':
         if musicDir[len(musicDir)-1] != "/":
             musicDir += "/"
     # Ensure path is valid, we don't want to start running rm operations on a bad directory
     assert os.path.exists(musicDir), "ERROR "+str(musicDir) + " is an invalid directory"
     return musicDir
+
+def collectSongs(musicDir, files = []):
+    # Walk the directory and build list of files
+    for (path, dirnames, filenames) in os.walk(musicDir):
+        files.extend(os.path.join(path, name) for name in sorted(filenames))
+    return files
+
+def mainLoop(musicDir, files = []):
+    for i in range(len(files)):
+        # I want the sox output to be yellow so I can distinguish from my python
+        print (Fore.YELLOW) 
+        # need to wrap file names in quotes so they are read properly
+        if system() == 'Windows':
+            #os.system("vlc --intf dummy " + "\"" + files[i] + "\"")
+            print("Sorry, windows development ongoing... Not working yet")
+            deinit() # need to stop colorama before exiting 
+            sys.exit()
+        elif system() =='Linux':
+            os.system("play " + "\"" + files[i] + "\"")
+        else:
+                print("\nCannot determine OS, exiting...\n")
+                deinit() # stop colorama before exiting
+                sys.exit()
+        # After keyboard interrupt:
+        actionOnFile(i)
+
+    deinit() #stop colorama
+
+def actionOnFile(i):
+    print(Style.RESET_ALL)
+    print("Name this file(x to delete, k to keep) do not include extension.")
+    uncheckedName = input("Name: ")
+    # pass the desired name to check it before continuing
+    songName = sanitizeSongName(uncheckedName)
+
+    if songName== "x":
+        cmd = ("rm " + "\"" + files[i] + "\"")
+        os.system(cmd)     
+        print(Fore.RED + "Deleted.")
+        print(Style.RESET_ALL)
+    elif songName == "k":
+        print(Fore.RED + "Keeping file as " + files[i])
+        print(Style.RESET_ALL)
+    else:
+        if files[i][len(files[i])-1] == "3": # mp3 file
+            songName = musicDir + songName + ".mp3"
+            renameSong(files[i], songName)
+        elif files[i][len(files[i])-1] == "v": # wav file
+            songName = musicDir + songName + ".wav"
+            renameSong(files[i], songName)
+        else:
+            print(Fore.RED + "Renaming to")
+
+def renameSong(pathToSong, newName):            
+    print(Fore.RED + "Renaming to " + newName)    # want to confirm rename in red
+    print(Style.RESET_ALL)
+    # mv bash command must recieve two arguments in quotes
+    cmd = ("mv " + "\"" + pathToSong + "\"" + " " + "\"" + newName + "\"")
+    os.system(cmd)
 
 def sanitizeSongName(nameToCheck):
     # ensure that name the user wants does not contain weird characters
@@ -48,60 +93,15 @@ def sanitizeSongName(nameToCheck):
     # multiple spaces are allowed but seriously.. don't do that
     keepGoing = True
     while(keepGoing):
-        y = re.search("^[a-zA-Z0-9\s]+$", nameToCheck)
-        if(y):
+        validName = search("^[a-zA-Z0-9\s]+$", nameToCheck)
+        if(validName):
             keepGoing = False
         else:
             print("Error this is not a valid name")
             nameToCheck = input("Please try again: ")
     return nameToCheck
 
-def playAndTakeInput(musicDir, files = []):
-    for i in range(len(files)):
-        # I want the sox output to be yellow so I can distinguish from my python
-        print (Fore.YELLOW) 
-        # need to wrap file names in quotes so they are read properly
-        if platform.system() == 'Windows':
-#            os.system("vlc --intf dummy " + "\"" + files[i] + "\"")
-            print("Sorry, windows development ongoing... Not working yet")
-            sys.exit()
-        elif platform.system() =='Linux':
-            os.system("play " + "\"" + files[i] + "\"")
-        else:
-                print("\nCannot determine OS, exiting...\n")
-                sys.exit()
-        print(Style.RESET_ALL)
-        print("Name this file(x to delete, k to keep) do not include extension.")
-        uncheckedName = input("Name: ")
-        # pass the desired name to check it before continuing
-        songName = sanitizeSongName(uncheckedName)
-
-        if songName== "x":
-                cmd = ("rm " + "\"" + files[i] + "\"")
-                os.system(cmd)     
-                print(Fore.RED + "Deleted.")
-                print(Style.RESET_ALL)
-        elif songName == "k":
-                print(Fore.RED + "Keeping file as " + files[i])
-                print(Style.RESET_ALL)
-        else:
-            if files[i][len(files[i])-1] == "3": # mp3 file
-                songName = musicDir + songName + ".mp3"
-                renameSong(files[i], songName)
-            elif files[i][len(files[i])-1] == "v": # wav file
-                songName = musicDir + songName + ".wav"
-                renameSong(files[i], songName)
-            else:
-                print(Fore.RED + "Renaming to") 
-    deinit() #stop colorama
-            
-def collectSongs(musicDir, files = []):
-    # Walk the directory and build list of files
-    for (path, dirnames, filenames) in os.walk(musicDir):
-        files.extend(os.path.join(path, name) for name in sorted(filenames))
-    return files
-
 musicDir = getMusicDirectory()
 files = []
 files = collectSongs(musicDir, files)
-playAndTakeInput(musicDir, files)
+mainLoop(musicDir, files)
