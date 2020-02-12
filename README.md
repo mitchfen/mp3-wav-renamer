644379eb9eee81db90eb1826cea6e1a0aa4e4e71
Mitchell Fenner <mitchfen@protonmail.com>
2020-02-12 15:12:43 -0500

    Improved filetype detection functionality

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # renameMyMusic.py
 
-I am writing this python script to help me organize my mess of a music library.  
+I am writing this python script to help me organize my mess of a music library.  This program reads each mp3 or wav into a list, then iterates through them and plays each one until a keyboard interrupt (CTRL + C). The user is then asked to rename the song, delete it, or keep it.
 
-This program reads each mp3 or wav into a list, then iterates through them and plays each one until a keyboard interrupt (CTRL + C).
-
-The user is then asked to rename the song, delete it, or keep it.
+The script will prevent invalid song names, invalid directories, and detect the right file extensions mp3 and wav.
 
 ## Dependencies
 
@@ -15,7 +13,6 @@ SoX: Debian and derivatives: `sudo apt-get install sox` or `sudo pacman -S sox` 
 
 * Windows support: SoX not workable on windows.
     * Have tried VLC without GUI but need way to cancel playback
-* Need to be able to detect redundant names to avoid overwrite
 * Add functionality to move to sub directories
 * Add output to see progress
 
--- renameMyMusic.py
+++ renameMyMusic.py
@@ -1,105 +1,128 @@
 import os
 import sys
-from pathlib import Path
-from platform import system
-from re import search
-from colorama import Fore, Style, deinit, init
+import platform
+import colorama
+import re
 
-init() # initialize colorama
+colorama.init() # per colorama documentation
 
 def getMusicDirectory():
+
     # Ask user where the music is stored
-    print (Fore.GREEN + "\nPlease enter the directory your music is in.")
+    print (colorama.Fore.GREEN + "\nPlease enter the directory your music is in.")
     print ("Enter it like: /home/username/music")
     print ("or C:\\Users\\mitch\\Music\n")
-    print(Style.RESET_ALL)
-    print (Fore.BLUE + "Directory: ", end = "")   # want input on same line, different color
-    print(Style.RESET_ALL, end = "")
+    print(colorama.Style.RESET_ALL)
+    print (colorama.Fore.BLUE + "Directory: ", end = "")   # want input on same line, different color
+    print(colorama.Style.RESET_ALL, end = "")
     musicDir = input()
+
     # Ensure directory terminates in correct symbol.
-    # Need a / on Unix, \ for Windows
-    if system() == 'Windows':
+    # Need to terminate in / on Windows
+    if platform.system() == 'Windows':
         if musicDir[len(musicDir)-1] != "\"":
             musicDir += "/"
-    if system() == 'Linux':
+    
+    # Need a / on Unix
+    if platform.system() == 'Linux':
         if musicDir[len(musicDir)-1] != "/":
             musicDir += "/"
+
     # Ensure path is valid, we don't want to start running rm operations on a bad directory
     assert os.path.exists(musicDir), "ERROR "+str(musicDir) + " is an invalid directory"
     return musicDir
 
 def collectSongs(musicDir, files = []):
+   
     # Walk the directory and build list of files
     for (path, dirnames, filenames) in os.walk(musicDir):
         files.extend(os.path.join(path, name) for name in sorted(filenames))
+    
+    # Remove files which are not music
+    # Will be faster to do an if statement inside the above loop
+    for i in range(len(files)):
+        if not files[i].endswith('.mp3') and not files[i].endswith('wav'):
+            files.pop(i) 
+
     return files
 
 def mainLoop(musicDir, files = []):
+
     for i in range(len(files)):
         # I want the sox output to be yellow so I can distinguish from my python
-        print (Fore.YELLOW) 
+        print (colorama.Fore.YELLOW) 
         # need to wrap file names in quotes so they are read properly
-        if system() == 'Windows':
+
+        if platform.system() == 'Windows':
             #os.system("vlc --intf dummy " + "\"" + files[i] + "\"")
             #os.system('cls')
             print("Sorry, windows development ongoing... Not working yet")
-            deinit() # need to stop colorama before exiting 
+            colorama.deinit() # need to stop colorama before exiting 
             sys.exit()
-        elif system() =='Linux':
+        elif platform.system() =='Linux':
             os.system('clear')
             os.system("play " + "\"" + files[i] + "\"")
         else:
                 print("\nCannot determine OS, exiting...\n")
-                deinit() # stop colorama before exiting
+                colorama.deinit() # stop colorama before exiting
                 sys.exit()
+
         # After keyboard interrupt:
         renamePrompt(i)
 
-    deinit() #stop colorama
+    # If user has provided input on all songs in the list, the program exits here
+    colorama.deinit() #stop colorama
 
 def renamePrompt(i):
-    print(Style.RESET_ALL)
+
+    # Get user input for the name of the file, do this in color white
+    print(colorama.Style.RESET_ALL)
     print("Name this file(x to delete, k to keep) do not include extension.")
     unsanitizedName = input("Name: ")
-    # pass the desired name to check it before continuing
+
     if unsanitizedName== "x":
         cmd = ("rm " + "\"" + files[i] + "\"")
         os.system(cmd)     
-        print(Fore.RED + "Deleted.")
-        print(Style.RESET_ALL)
+        print(colorama.Fore.RED + "Deleted.")
+        print(colorama.Style.RESET_ALL)
     elif unsanitizedName== "k":
-        print(Fore.RED + "Keeping file as " + files[i])
-        print(Style.RESET_ALL)
+        print(colorama.Fore.RED + "Keeping file as " + files[i])
+        print(colorama.Style.RESET_ALL)
+    # if the name is not k or x, it must be sanitized to remove invalid characters before renaming
     else:
-            newName = sanitizeSongName(unsanitizedName)
-            renameSong(files[i], newName)
+        newName = sanitizeSongName(unsanitizedName)
+        renameSong(files[i], newName)
 
 def renameSong(pathToSong, newName):            
-    # Add appropriate extension to the song
-    if pathToSong[len(pathToSong)-1] =="3": # mp3 file
+
+    if pathToSong.endswith('.mp3'):
         newName = musicDir + newName +".mp3"
-    elif pathToSong[len(pathToSong)-1] =="v": # wav file
+    elif pathToSong.endswith('.wav'):
         newName = musicDir + newName +".wav"
-    # Detect if song with that name already present
-    AlreadyExists = os.path.exists(newName)
-    if (AlreadyExists):
+    
+    # Detect if file with same name already exists
+    # NOTE: This intentionally allows both song.mp3 and song.wav to exist. 
+    #               Such a scenario is not detected
+    if os.path.exists(newName):
         secondaryName = input("Name already in use, please rename: ")
         sanitizeSongName(secondaryName)
         renameSong(pathToSong, secondaryName)
+
+    # If the song name is unique, then we print out its new name in red
+    # Then use mv command to rename file
     else:  
-        # want to confirm rename in red
-        print(Fore.RED + "Renaming to " + newName)  
-        print(Style.RESET_ALL)
+        print(colorama.Fore.RED + "Renaming to " + newName)  
+        print(colorama.Style.RESET_ALL)
         cmd = ("mv " + "\"" + pathToSong + "\"" + " " + "\"" + newName + "\"")
         os.system(cmd)
 
 def sanitizeSongName(nameToCheck):
-    # ensure that name the user wants does not contain weird characters
+
+    # ensure that name the user wants does not contain invalid characters
     # only spaces and alphanumerics are allowed
-    # multiple spaces are allowed but seriously.. don't do that
     keepGoing = True
     while(keepGoing):
-        validName = search("^[a-zA-Z0-9\s]+$", nameToCheck)
+        validName = re.search("^[a-zA-Z0-9\s]+$", nameToCheck)
         if(validName):
             keepGoing = False
         else:
